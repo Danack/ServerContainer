0c3fdc2714fcc1d6598dcc1522c8ba0a51170343
Danack <Danack@basereality.com>
2015-01-22 16:00:31 +0000

    Moved data file out of scripts directory.

--- scripts/build/addBaserealityRPMRepo.sh
+++ scripts/build/addBaserealityRPMRepo.sh
@@ -2,7 +2,7 @@ rpmPath="/etc/yum.repos.d/basereality.repo"
 
 if [ ! -f $rpmPath ]; then
     echo "basereality.repo not found in /etc/yum.repos.d, adding it."
-    cp ./basereality.repo $rpmPath
+    cp /home/servercontainer/data/basereality.repo $rpmPath
     chmod 00644 $rpmPath
     chown root:root $rpmPath
 fi
