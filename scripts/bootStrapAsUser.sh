bd96372ad3b8257e4165712c2a0e7bf561b0bd79
Danack <Danack@basereality.com>
2015-09-19 14:24:18 +0000

    Make the directory so file can be written.

--- scripts/bootStrapAsUser.sh
+++ scripts/bootStrapAsUser.sh
@@ -1,6 +1,5 @@
-# set -eux -o pipefail
-
-set -x
+set -eux -o pipefail
+#set -ex
 
 oauthtoken=`php bin/info.php "github.access_token"`
 composer config -g github-oauth.github.com $oauthtoken
@@ -8,5 +7,6 @@ composer config -g github-oauth.github.com $oauthtoken
 composerpath=`which composer`
 php -d allow_url_fopen=1 $composerpath install
 
+mkdir  autogen
 php vendor/bin/configurate -p data/config.php data/my.cnf.php autogen/my.cnf.conf amazonec2
 php vendor/bin/configurate -p data/config.php data/addConfig.sh.php autogen/addConfig.sh amazonec2
