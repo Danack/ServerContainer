40b6ba2010453207484dd2cba5d18843f338e5b7
Danack <Danack@basereality.com>
2015-10-09 15:46:49 +0000

    Fixed path.

--- scripts/build/addBaserealityRPMRepo.sh
+++ scripts/build/addBaserealityRPMRepo.sh
@@ -4,7 +4,7 @@ rpmPath="/etc/yum.repos.d/basereality.repo"
 
 if [ ! -f $rpmPath ]; then
     echo "basereality.repo not found in /etc/yum.repos.d, adding it."
-    cp ../data/basereality.repo $rpmPath
+    cp ./data/basereality.repo $rpmPath
     chmod 00644 $rpmPath
     chown root:root $rpmPath
 fi
@@ -16,7 +16,7 @@ proxyPath="/etc/yum.repos.d/yum.proxy.conf"
 
 if [ ! -f $rpmPath ]; then
     echo "yum.proxy.conf not found in /etc/yum.repos.d, adding it."
-    cp ../data/yum.proxy.conf $proxyPath
+    cp ./data/yum.proxy.conf $proxyPath
     chmod 00644 $proxyPath
     chown root:root $proxyPath
 fi
